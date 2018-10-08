37e122888963ee9a90528ae0fe3cab2655ba62bf
Dana Iti <dana.iti.work@gmail.com>
2018-10-08 15:30:51 +1300

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# piccolo
+# Picollo
 International Legislation as Code exploration
+
+
+## How to configure and run
+
+`npm install`
+
+`npm start`
+
+# Run Tests
+
+`npm test`
---
+++ src/container/app-container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import App from '../components/App';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/index';
+
+class AppCon extends Component {
+  render() {
+    return (
+      <div>
+        <App />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => state;
+
+export default connect(mapStateToProps, actionCreators)(AppCon);
