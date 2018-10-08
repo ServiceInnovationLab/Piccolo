0613b7879e5427a0e12848253b19ed2394faf0ae
Awesome Code <team@xinminlabs.com>
2018-10-08 21:28:11 +0000

    Auto corrected by following Javascript sort-imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { HashRouter, Route } from 'react-router-dom';
