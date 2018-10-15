ec34cede8481595d79ec8f916b18c7ab5de102ec
Dana Iti <dana.iti.work@gmail.com>
2018-10-16 12:57:11 +1300

    added section, button and removed margin on body

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/index';
 import '../styles/App.css';
+import { Textfield } from '../elements/Input';
+import { Button } from '../elements/Button';
+import { Section } from '../elements/Section';
 
 class App extends Component {
   render() {
-    return (
-      <p>International Legislation as Code exploration</p>
-    );
+    return <Fragment><p>International Legislation as Code exploration</p>
+      <Button>Calculate</Button>
+      <Section dark>BLABLABLA</Section>
+      <Textfield /></Fragment>;
   }
 }
 
