82c83b7ba5bcdefce76c3f288d197b9e35fb5d82
Dana Iti <dana.iti.work@gmail.com>
2018-10-16 12:06:06 +1300

    added focus and attributes for textfield

--- src/elements/input.js
+++ src/elements/input.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
-export const Input = styled.input`
-  border: 4px solid #42e2ee;
+export const Textfield = styled.input.attrs({ type: 'text'})`
+  border: 4px solid #d1d1d1;
   border-radius: 10px;
   height: 40px;
   width: 100%;
+  &:focus {
+    border: 4px solid #42e2ee;
+  }
 `;
