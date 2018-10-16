88e1fa66ec930c3d9f84ebb47bbe6c3ba87b68ef
Dana Iti <dana.iti.work@gmail.com>
2018-10-17 12:02:57 +1300

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/index';
 import '../styles/App.css';
-import { Textfield } from '../elements/Input';
+import { TextField } from '../elements/Input';
 import { Button } from '../elements/Button';
 import { Section } from '../elements/Section';
 import { Container } from '../elements/Container';
@@ -11,26 +11,33 @@ import { CreativeCommons } from './CreativeCommons';
 
 class App extends Component {
   render() {
-    return <Container><p>International Legislation as Code exploration</p>
+    return <Container>
+      <p>International Legislation as Code exploration</p>
       <CreativeCommons label="Content about creative commons"/>
+
       <Button>Calculate</Button>
+
       <List stripped>
         <Item>About this website</Item>
         <Item>Contact Us</Item>
       </List>
+
       <Section dark>
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
       </Section>
+
       <Section light>
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
       </Section>
+
       <Section grey>
         <h2>Heading 2</h2>
         <h3>Heading 3</h3>
       </Section>
-      <Textfield />
+
+      <TextField />
     </Container>;
   }
 }
--- src/elements/List.js
+++ src/elements/List.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const List = styled.ul`
-  list-style: ${props => props.stripped ? 'none' : ''}
-  padding-left: ${props => props.stripped ? 0 : ''}
+  list-style: ${props => props.stripped ? 'none' : ''};
+  padding-left: ${props => props.stripped ? 0 : ''};
   border-top: 3px solid #000;
   padding-top: 12px;
 `;
