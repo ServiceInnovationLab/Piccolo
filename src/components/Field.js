7ad83e8aba88ffb15f75690ed3ca0bfc488be3ee
Dana Iti <dana.iti.work@gmail.com>
2018-10-18 15:32:12 +1300

    remove reference to css

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 import TextField from '../elements/TextField';
 import RadioGroup from '../elements/RadioGroup';
 
