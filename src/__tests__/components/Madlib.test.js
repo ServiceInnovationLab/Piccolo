25f4322bd748f75925bb1bdd730f02e31f92ec06
dlouise64 <dana.iti.work@gmail.com>
2018-11-01 11:20:20 +1300

    update madlib test

--- src/__tests__/components/Madlib.test.js
+++ src/__tests__/components/Madlib.test.js
@@ -4,11 +4,12 @@ import Madlib from '../../components/Madlib';
 import 'jest-styled-components';
 
 test('Madlib renders correctly', () => {
+
   let values = {
-    age: '55',
-    years_worked: '55',
-    years_lived_in_country: '40',
-    number_of_children: '5',
+    age: 55,
+    years_worked: 55,
+    years_lived_in_country: 40,
+    number_of_children: 5,
     gender: 'woman'
   };
   const tree = renderer.create(<Madlib values={values} />).toJSON();
