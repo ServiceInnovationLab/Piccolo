aba78f1d0ff3a0a32766111458f661dadc0e912f
Awesome Code <team@xinminlabs.com>
2018-10-31 03:57:02 +0000

    Auto corrected by following Javascript sort-imports

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,10 @@ import Form from '../components/Form';
 import Section from '../elements/Section';
 import TextField from '../elements/TextField';
 import {
-  SectionOne,
-  SectionThree,
+  SectionFive,
   SectionFour,
-  SectionFive
+  SectionOne,
+  SectionThree
 } from '../page/Sections';
 import countries from '../data/countries';
 import axios from 'axios';
@@ -19,9 +19,9 @@ import DATA from '../data/Data';
 import Container from '../elements/Container';
 
 import {
-  RadioGroupWrapper,
-  RadioGroupInner,
   Radio,
+  RadioGroupInner,
+  RadioGroupWrapper,
   RadioLabel,
   RadioSpan
 } from '../elements/RadioGroup';
