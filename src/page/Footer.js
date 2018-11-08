0f50cd2eb69a8259c95ebb0a2d50c31d4415f6ef
Siobhan McCarthy <36939214+ssibbehh@users.noreply.github.com>
2018-11-09 10:01:21 +1300

    Footer changes (#197)
    * Removed "about this website" and added email link to contact
    
    Needs styling on the link
    
    * add some styles

--- src/page/Footer.js
+++ src/page/Footer.js
@@ -8,6 +8,14 @@ const Footer = styled.section`
   padding-top: 45px;
   height: 270px;
   background: #eee;
+
+  a {
+    display: inline-block;
+    color: #000;
+    margin-top: 20px;
+    text-decoration: none;
+    padding-bottom: 5px;
+  }
 `;
 
 
@@ -16,8 +24,7 @@ const PageFooter = () => (
     <Footer>
       <Container>
         <List stripped bordered>
-          <Item>About this website</Item>
-          <Item>Contact Us</Item>
+          <Item><a href="mailto:serviceinnovationlab@dia.govt.nz">Contact Us</a></Item>
         </List>
 
         <CreativeCommons label="Creative Commons"/>
