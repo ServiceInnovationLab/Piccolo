111a150330ee217de28d37d38c11728760bb8498
Dana Iti <dana.iti.work@gmail.com>
2018-11-08 14:26:04 +1300

    remove commented code

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -26,10 +26,6 @@ export const Column = styled.div`
     padding-bottom: 0;
   }
 
-  // @media(min-width: 325px) {
-  //   padding: 0 40px 10px 40px;
-  // }
-
   @media(min-width: 1024px) {
     flex: 1;
     border-left: 1px solid #000;
