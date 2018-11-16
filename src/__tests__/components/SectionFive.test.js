7b05b04bbc9b295641946e0ba6402864a840ef65
Awesome Code <team@xinminlabs.com>
2018-11-16 08:28:57 +0000

    Auto corrected by following Javascript quote-props

--- src/__tests__/components/SectionFive.test.js
+++ src/__tests__/components/SectionFive.test.js
@@ -5,107 +5,107 @@ import 'jest-styled-components';
 
 test('SectionFive renders correctly', () => {
   let israel = {
-    'data': {
-      'households': {
-        'one': {
-          'parents': [
+    data: {
+      households: {
+        one: {
+          parents: [
             'Tahi'
           ]
         }
       },
-      'persons': {
-        'Tahi': {
-          'born_in_israel': {
+      persons: {
+        Tahi: {
+          born_in_israel: {
             '2018-08': true
           },
-          'gender': {
+          gender: {
             '2018-08': 'woman'
           },
-          'is_resident': {
+          is_resident: {
             '2018-08': true
           },
-          'pension_contributing_years': {
+          pension_contributing_years: {
             '2018-08': 66
           },
-          'pension_eligibility_age': {
+          pension_eligibility_age: {
             '2018-08': 62
           },
-          'eligible_for_pension': {
+          eligible_for_pension: {
             '2018-08': true
           }
         }
       }
     },
-    'status': 200
+    status: 200
   };
   let new_zealand = {
-    'data': {
-      'families': {
-        'family': {
-          'others': [
+    data: {
+      families: {
+        family: {
+          others: [
             'Tahi'
           ]
         }
       },
-      'persons': {
-        'Tahi': {
-          'is_permanent_resident': {
+      persons: {
+        Tahi: {
+          is_permanent_resident: {
             '2018-08': true
           },
-          'super__eligibility': {
+          super__eligibility: {
             '2018-08': true
           },
-          'super___eligibility_age': {
+          super___eligibility_age: {
             '2018-08': 65
           },
-          'total_number_of_years_lived_in_nz_since_age_20': {
+          total_number_of_years_lived_in_nz_since_age_20: {
             '2018-08': 66
           },
-          'total_number_of_years_lived_in_nz_since_age_50': {
+          total_number_of_years_lived_in_nz_since_age_50: {
             '2018-08': 66
           }
         }
       },
-      'titled_properties': {
-        'house': {
-          'others': [
+      titled_properties: {
+        house: {
+          others: [
             'Tahi'
           ]
         }
       }
     },
-    'status': 200
+    status: 200
   };
   let uruguay = {
-    'data': {
-      'households': {
-        'one': {
-          'parents': [
+    data: {
+      households: {
+        one: {
+          parents: [
             'Tahi'
           ]
         }
       },
-      'persons': {
-        'Tahi': {
-          'edad_de_jubilacion': {
+      persons: {
+        Tahi: {
+          edad_de_jubilacion: {
             '2018-08': 0
           },
-          'eligible_for_pension': {
+          eligible_for_pension: {
             '2018-08': false
           },
-          'gender': {
+          gender: {
             '2018-08': 'woman'
           },
-          'number_of_children': {
+          number_of_children: {
             '2018-08': 66
           },
-          'number_of_years_worked': {
+          number_of_years_worked: {
             '2018-08': 66
           }
         }
       }
     },
-    'status': 200
+    status: 200
   };
 
   const tree = renderer.create(<SectionFive
